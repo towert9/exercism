4a339480ef133610c9b9ad6249f6954e9c2a8960
Unknown <towert440@gmail.com>
2018-06-03 22:48:02 +0900

    leap 문제 완료

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,16 +1,7 @@
 var TwoFer = function () { };
 
-String.prototype.format = function () {
-  var content = this;
-  for (var i = 0; i < arguments.length; ++i) {
-    var replacement = '{' + i + '}';
-    content = content.replace(replacement, arguments[i]);
-  }
-  return content;
-}
-
-TwoFer.prototype.twoFer = function (who) {
-  return 'One for {0}, one for me.'.format(who == null ? 'you' : who);
+TwoFer.prototype.twoFer = function (who = 'you') {
+  return `One for ${who}, one for me.`;
 };
 
 module.exports = TwoFer;
